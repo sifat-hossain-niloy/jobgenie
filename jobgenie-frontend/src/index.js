a5e3e4966e2d9b13ec42e755b4daa1e4e4128f30
AhnafTahmiddie <ahnaftahmid2604@gmail.com>
2024-04-25 01:02:19 +0600

    router updated to use useLoaderData

--- jobgenie-frontend/src/index.js
+++ jobgenie-frontend/src/index.js
@@ -8,9 +8,9 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Suspense>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
       <App />
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
   </Suspense>
 );
 
